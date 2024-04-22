821ad324014e45229055bcd3a2caceed947b8310
cri98li <cri98li@gmail.com>
2024-04-22 16:09:55 +0300

    Added the firstPoint normalization

--- geoletrld/utils/Trajectory.py
+++ geoletrld/utils/Trajectory.py
@@ -17,3 +17,19 @@ class Trajectory:
 
     def copy(self):
         return Trajectory(values=self.values)
+
+    def _first_point_normalize(self, inplace=True):
+        if not inplace:
+            return Trajectory(values=self.values)._first_point_normalize(inplace=True)
+
+        self.latitude -= self.latitude[0]
+        self.longitude -= self.longitude[0]
+        self.time -= self.time[0]
+
+        return self
+    def normalize(self, type='FirstPoint', inplace=True):
+        if type == 'FirstPoint':
+            return self._first_point_normalize(inplace=inplace)
+        else:
+            raise ValueError(f"Unknown normalization '{type}'")
+
