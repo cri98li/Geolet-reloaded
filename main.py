0c347571c076d0d6a069d766ce747ed673364fc8
cri98li <cri98li@gmail.com>
2024-06-18 18:00:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,57 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm.auto as tqdm
+from sklearn.cluster import KMeans, AffinityPropagation, OPTICS, SpectralClustering
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score, f1_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn_extra.cluster import KMedoids
 from sklearn.tree import DecisionTreeClassifier
 
+from geoletrld.classifier import GeoletClassifier
+from geoletrld.selectors import RandomSelector, MutualInformationSelector, SelectorPipeline, ClusteringSelector, \
+    GapSelector
 from geoletrld.utils import Trajectories, y_from_df
 from geoletrld.partitioners import NoPartitioner, GeohashPartitioner, FeaturePartitioner, SlidingWindowPartitioner
-from geoletrld.distances import EuclideanDistance
+from geoletrld.distances import EuclideanDistance, InterpolatedTimeDistance, LCSSTrajectoryDistance, FrechetDistance, CaGeoDistance
+
 
 if __name__ == "__main__":
-    df = pd.read_csv('datasets/animals.zip')
+    df = pd.read_csv('datasets/vehicles.zip')
+    df[["c1", "c2"]] /= 10**5
+    #df = pd.read_csv("datasets/animals.zip")
 
     y = y_from_df(df, tid_name="tid", y_name="class")
 
     trajectories = Trajectories.from_DataFrame(df, latitude="c1", longitude="c2", time="t")
 
-    geolets = NoPartitioner().transform(trajectories)
+    X_train, X_test, y_train, y_test = train_test_split(list(trajectories), y, test_size=0.3, random_state=32,
+                                                        stratify=y)
+    X_train = Trajectories([(k, trajectories[k]) for k in X_train])
+    X_test = Trajectories([(k, trajectories[k]) for k in X_test])
+
+    classifier = GeoletClassifier(
+        partitioner=GeohashPartitioner(precision=6),
+        selector=SelectorPipeline(
+            RandomSelector(k=500),
+            #MutualInformationSelector(n_jobs=8, k=30)
+            #GapSelector(k=5, n_jobs=10),
+            ClusteringSelector(
+                #KMeans(n_clusters=5), #è sbagliato?, ma funziona stranamente bene
+                KMedoids(n_clusters=5, metric='precomputed'), #distance=RotatingEuclideanDistance(verbose=True), n_jobs=9
+                #AffinityPropagation(affinity="precomputed"), agg=lambda x: -np.sum(x),
+                #OPTICS(metric="precomputed"),
+                #SpectralClustering(affinity="precomputed"), agg=lambda x: -np.sum(x), #non gira
+            )
+        ),
+        distance=CaGeoDistance(n_jobs=10, verbose=True), #LCSSTrajectoryDistance(n_jobs=1, verbose=True),#RotatingEuclideanDistance(verbose=True, n_jobs=9, return_rot=True), #EuclideanDistance()
+        model_to_fit=RandomForestClassifier(n_estimators=500, class_weight="balanced", random_state=32),
+    ).fit(X_train, y_train)
+
+    print("Random Forest:\n", classification_report(y_test, classifier.predict(X_test)))
+
+    """geolets = NoPartitioner().transform(trajectories)
     print(geolets)
 
     geolets = GeohashPartitioner(precision=3).transform(trajectories)
@@ -27,19 +61,23 @@ if __name__ == "__main__":
     geolets = FeaturePartitioner(feature="distance", threshold=.2).transform(trajectories)
     print(geolets)
 
-    geolets = SlidingWindowPartitioner(window_size=10).transform(trajectories)
+    geolets = SlidingWindowPartitioner(window_size=30).transform(trajectories)
     print(geolets)
 
-    best_dist, best_idx = EuclideanDistance(agg=np.sum, n_jobs=8, verbose=True)\
+    best_dist, best_idx = EuclideanDistance(agg=np.sum, n_jobs=8, verbose=True) \
         .transform(trajectories=trajectories, geolets=geolets)
 
-    print(best_dist)
+    #print(best_dist)
 
+    #best_dist, best_idx = InterpolatedTimeDistance(agg=np.sum, n_jobs=1, verbose=True) \
+    #    .transform(trajectories=trajectories, geolets=geolets)
+
+    print(best_dist)
+    
     X_train, X_test, y_train, y_test = train_test_split(best_dist, y, test_size=0.3, random_state=42, stratify=y)
 
     dt = DecisionTreeClassifier().fit(X_train, y_train)
     print("Decision Tree:\n", classification_report(y_test, dt.predict(X_test)))
 
-    rf = RandomForestClassifier(n_estimators=500).fit(X_train, y_train)
-    print("Decision Tree:\n", classification_report(y_test, rf.predict(X_test)))
-
+    rf = RandomForestClassifier().fit(X_train, y_train)
+    print("Random Forest:\n", classification_report(y_test, rf.predict(X_test)))"""
