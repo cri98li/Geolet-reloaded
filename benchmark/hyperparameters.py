e73d3ea3b3d38b2ff73bc522474459f3baee3f1a
cri98li <c.landi7@studenti.unipi.it>
2024-08-10 21:43:32 +0200

    Update hyperparameters.py

--- benchmark/hyperparameters.py
+++ benchmark/hyperparameters.py
@@ -22,14 +22,13 @@ def get_hyperparameters(n_jobs=-1):
     distance_hyper = \
         [dist.EuclideanDistance(agg=x, n_jobs=n_jobs) for x in distance_agg_fun] + \
         [dist.CaGeoDistance(n_gaps=gaps, agg=agg, n_jobs=n_jobs) for gaps, agg in
-         product([1, 3, 5], [np.sum, cosine_distance])] + \
+         product([1, 3], [np.sum, cosine_distance])] + \
         [dist.FrechetDistance(n_jobs=n_jobs)] + \
         [dist.InterpolatedTimeDistance(agg=agg, n_jobs=n_jobs) for agg in [np.sum, np.mean]] + \
-        [dist.LCSSTrajectoryDistance(max_dist=d, max_time=t, n_jobs=n_jobs) for d, t in product([1, 10, 100, 1000],
+        [dist.LCSSTrajectoryDistance(max_dist=d, max_time=t, n_jobs=n_jobs) for d, t in product([10, 100, 1000],
                                                                                                 [10, 30, 60])] + \
         [dist.RotatingGenericDistance(distance=d, n_jobs=n_jobs)
-         for d in [dist.EuclideanDistance(),
-                   dist.InterpolatedTimeDistance(n_jobs=n_jobs)]] + \
+         for d in [dist.EuclideanDistance(), dist.InterpolatedTimeDistance(n_jobs=n_jobs)]] + \
         [dist.MatchComputeDistance(distance1=bf1, distance2=bf2, n_jobs=n_jobs) for bf1, bf2 in product(
             [
                 dist.RotatingGenericDistance(distance=dist.EuclideanDistance(agg=np.mean)),
@@ -40,22 +39,21 @@ def get_hyperparameters(n_jobs=-1):
                 dist.FrechetDistance(),
                 dist.InterpolatedTimeDistance(n_jobs=n_jobs),
             ] +
-            [dist.CaGeoDistance(n_gaps=gaps, agg=agg)
-             for gaps, agg in product([1, 3, 5], [np.sum, cosine_distance])]
+            [dist.CaGeoDistance(n_gaps=gaps, agg=agg) for gaps, agg in product([1], [np.sum, cosine_distance])]
         )]
 
     partition_hyper = \
-        [part.GeohashPartitioner(precision=p) for p in range(2, 7 + 1)] + \
+        [part.GeohashPartitioner(precision=p) for p in range(4, 7 + 1)] + \
         [part.FeaturePartitioner(feature=f, threshold=t, overlapping=o)
          for f, t, o in product(
             ["time", "distance"],
-            [10, 60, 100, 500],
+            [60, 100, 500],
             [True]
         )] + \
         [part.FeaturePartitioner(feature=f, threshold=t, overlapping=o)
          for f, t, o in product(
             ["speed", "acceleration"],
-            [5, 10, 50, 100],
+            [10, 50, 100],
             [True]
         )] + \
         [part.NoPartitioner()] + \
@@ -67,10 +65,10 @@ def get_hyperparameters(n_jobs=-1):
         [sel.ClusteringSelector(clustering_fun=KMedoids(n_clusters=k, metric="precomputed"), n_jobs=n_jobs)
          for k in n_geolets] + \
         [sel.GapSelector(k=k, n_jobs=n_jobs) for k in n_geolets] + \
-        [sel.RandomSelector(k=k) for k in n_geolets] + \
-        [sel.ClusteringSelector(clustering_fun=AffinityPropagation(affinity="precomputed"), use_sim=True,
-                                n_jobs=n_jobs)] + \
-        [sel.ClusteringSelector(clustering_fun=SpectralClustering(affinity="precomputed"), use_sim=True, n_jobs=n_jobs)]
+        [sel.RandomSelector(k=k) for k in n_geolets]  #+ \
+    #[sel.ClusteringSelector(clustering_fun=AffinityPropagation(affinity="precomputed"), use_sim=True,
+    #                        n_jobs=n_jobs)] + \
+    #[sel.ClusteringSelector(clustering_fun=SpectralClustering(affinity="precomputed"), use_sim=True, n_jobs=n_jobs)]
 
     select_hyper_sup = [sel.MutualInformationSelector(k=k) for k in n_geolets]
 
@@ -79,14 +77,14 @@ def get_hyperparameters(n_jobs=-1):
             "partitioner": partition_hyper,
             "selector": select_hyper_unsup + select_hyper_sup,
             "distance": distance_hyper,
-            "subset_candidate_geolet": [100, 500, 1000],
-            "subset_trj_in_selection": [50, 100, 500]
+            "subset_candidate_geolet": [100, 500],
+            "subset_trj_in_selection": [50, 100]
         },
         'clu': {
             "partitioner": partition_hyper,
             "selector": select_hyper_unsup,
             "distance": distance_hyper,
-            "subset_candidate_geolet": [100, 500, 1000],
-            "subset_trj_in_selection": [50, 100, 500]
+            "subset_candidate_geolet": [100, 500],
+            "subset_trj_in_selection": [50, 100]
         }
     }
